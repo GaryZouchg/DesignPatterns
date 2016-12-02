81f091380351afb0fed2d8d34a0272a7b101b1d5
Gary <522702305@qq.com>
2016-12-02 17:15:33 +0800

    builder change

--- Builder/Builder.cpp
+++ Builder/Builder.cpp
@@ -15,11 +15,12 @@ int _tmain(int argc, _TCHAR* argv[])
 	reader.construct(input2, UNM_ENTRIES);
 	std::cout<< unixbuilder.getResult()->getState()<<std::endl;
     //unixbuilder.getResult()返回 DistrWorkPackage
-    //Reader的对象来创建 DistrWorkPackage 对象
-	reader.setBuilder(&vmsbuilder);
+    //通过Reader的对象来创建 DistrWorkPackage 对象
+	
+    reader.setBuilder(&vmsbuilder);//通过不同的builder创建不同的对象
 	reader.construct(input,UNM_ENTRIES);
 	std::cout << vmsbuilder.getResult()->getState()<<std::endl;
-
+    
 	std::cin >> exit;
 	return 0;
 }
